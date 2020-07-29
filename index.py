0f7bff34b91367eb515a9482586d7357eef71a32
Boban Bogoevski <boban.bogoevski@students.finki.ukim.mk>
2020-07-29 19:15:02 +0200

    Added RF classifier and bar plot

--- index.py
+++ index.py
@@ -6,7 +6,8 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 from sklearn.naive_bayes import CategoricalNB
 from sklearn.preprocessing import OrdinalEncoder
-
+from sklearn.metrics import f1_score
+from sklearn.ensemble import RandomForestClassifier
 
 def readFullData():
     #Podatocite se smestuvaat vo tn. hash mappa (dict vo python) so vrednost na kluchot po imeto na mesecot
@@ -21,28 +22,25 @@ def readFullData():
 
 
 def barPlot(data_x, data_y):
+    #Sepak poarno bilo so seaborn barplot, ama dolgo vreme trae za sekoja kolona da napravi plot
     plt.figure()
-    plt.bar(data_x,data_y)
+    sns.barplot(data_x,data_y)
     plt.show()
-    # unique_x_values = set(data_x.to_numpy())
-    # uniq_x_val_count = []
-    # for val in unique_x_values:
 
-
-def statisticalInfo(data:pd.DataFrame,target_column_name):
+def statisticalInfo(data:pd.DataFrame):
     corr_matrix = data.corr()
 
     sns.heatmap(corr_matrix)
     plt.title("Heatmap of the correlation between columns")
     plt.show()
 
-    column_names = data.columns[:-1]
-    data_y = data[target_column_name]
-    # Malce podolgo kje trae za barplot ama ne mi se svigja kako gi pretstavuva taka da kje probam rachno ubavo
-    # da go napravam
-    # for c in column_names:
-    #     data_x = data[c]
-    #     barPlot(data_x,data_y)
+    column_names = list(data.columns)[:-1]
+    column_names.remove("ARR_DEL15")
+    data_y = data["ARR_DEL15"]
+
+    for c in column_names:
+        data_x = data[c]
+        barPlot(data_x,data_y)
 
 def getDataset(dataset):
     dataset = [row[:-1] for row in dataset]
@@ -68,10 +66,66 @@ def getTestingSet(fullData):
     testingSet = getDataset(testingSet)
     return testingSet
 
+
+def randomForestClassification(X, Y, test_set_x, test_set_y):
+    max_depths = [3,5,8,12]
+    n_estimators = [10, 30, 50, 80, 100]
+    y_estimators = dict()
+    y_estimators["gini"] = []
+    y_estimators["entropy"] = []
+    for n in n_estimators:
+        rf_g = RandomForestClassifier(n,"gini")
+        rf_e = RandomForestClassifier(n,"entropy")
+        rf_g.fit(X,Y)
+        rf_e.fit(X, Y)
+        predictions = rf_g.predict(test_set_x)
+        #Zemam f1 score bidejki zema vo predvid i promashenite klasifikacii
+        y_estimators["gini"].append(f1_score(test_set_y,predictions))
+        predictions = rf_e.predict(test_set_x)
+        y_estimators["entropy"].append(f1_score(test_set_y, predictions))
+        print("Finished prediction for no. of estimators",n)
+    plt.figure()
+    plt.title("F1 score measured by no. of estimators")
+    plt.xlabel("No. of estimators")
+    plt.ylabel("F1 Score")
+    g_plot, = plt.plot(n_estimators,y_estimators["gini"],color="r")
+    e_plot, = plt.plot(n_estimators,y_estimators["entropy"],color="b")
+    plt.legend([g_plot,e_plot],["Gini","Entropy"])
+    plt.show()
+
+    #Posho se gleda deka so 100 estimatori bez ogranichuvanje na dlabochinata dava najdobar rezultat, zemame 100 za
+    #broj na estimatori za slednava proverka
+    y_depths = dict()
+    y_depths["gini"] = []
+    y_depths["entropy"] = []
+    for depth in max_depths:
+
+        rf_g = RandomForestClassifier(n_estimators=100, criterion="gini",max_depth=depth)
+        rf_e = RandomForestClassifier(n_estimators=100, criterion="entropy",max_depth=depth)
+        rf_g.fit(X, Y)
+        rf_e.fit(X, Y)
+        predictions = rf_g.predict(test_set_x)
+        # Zemam f1 score bidejki zema vo predvid i promashenite klasifikacii
+        y_depths["gini"].append(f1_score(test_set_y, predictions))
+        predictions = rf_e.predict(test_set_x)
+        y_depths["entropy"].append(f1_score(test_set_y, predictions))
+        print("Finished prediction for depth", depth)
+
+    plt.figure()
+    plt.title("F1 score measured by max depth")
+    plt.xlabel("Max Depth")
+    plt.ylabel("F1 Score")
+    g_plot, = plt.plot(n_estimators, y_depths["gini"], color="r")
+    e_plot, = plt.plot(n_estimators, y_depths["entropy"], color="b")
+    plt.legend([g_plot, e_plot], ["Gini", "Entropy"])
+    plt.show()
+
+
+
 if __name__ == '__main__':
     full_data = readFullData()
-    #pd.concat(full_data.values()) #ti e ustvari spojuvanjeto na site podatoci
-    #statisticalInfo(pd.concat(full_data.values()),'ARR_DEL15')
+
+    # statisticalInfo(pd.concat(full_data.values()))
     trainingSet = getTrainingSet(full_data)
     testingSet = getTestingSet(full_data)
     dataset = trainingSet + testingSet
@@ -87,6 +141,7 @@ if __name__ == '__main__':
     classifier.fit(X, Y)
 
     test_set_x = encoder.transform([row[:-1] for row in testingSet])
+    print(np.isnan(test_set_x).any())
     test_set_y = [row[-1] for row in testingSet]
     predictions = classifier.predict(test_set_x)
 
@@ -98,4 +153,8 @@ if __name__ == '__main__':
 
     accuracy = right/len(testingSet)
     print(accuracy)
+    print(f1_score(test_set_y,predictions))
+
+    randomForestClassification(X,Y,test_set_x,test_set_y)
+
 
